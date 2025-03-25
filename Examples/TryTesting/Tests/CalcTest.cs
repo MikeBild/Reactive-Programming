db5c9c135d44c430ad728b44b50dfc005198fad5
Mike Bild <mike.bild@siemens.com>
2025-03-25 12:02:28 +0100

    add theory

--- Examples/TryTesting/Tests/CalcTest.cs
+++ Examples/TryTesting/Tests/CalcTest.cs
@@ -12,4 +12,17 @@ public class CalcTest
         // Assert
         Assert.Equal(3, actual);
     }
+
+    [Theory]
+    [InlineData(1, 2, 3)]
+    [InlineData(5, 5, 10)]
+    public void Add_DataDriven(int a, int b, int expected)
+    {
+        // Arrange
+        // Act
+        var actual = Calc.Add(a, b);
+
+        // Assert
+        Assert.Equal(expected, actual);
+    }
 }
